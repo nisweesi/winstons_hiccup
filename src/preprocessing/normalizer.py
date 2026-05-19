6d006b5b33cc85dbad49df2b52ddf1b9ca63faf5
Nadir Isweesi <isweesin@oregonstate.edu>
2026-05-19 13:35:32 -0700

    add tokenization and normilzation

---
+++ src/preprocessing/normalizer.py
@@ -0,0 +1,10 @@
+def normalize(doc):
+    tokens = [
+        "<NUM>" if token.like_num else token.lemma_.lower()
+        for token in doc
+        if token.is_alpha
+        and not token.is_stop
+        and not token.is_punct
+    ]
+
+    return tokens
